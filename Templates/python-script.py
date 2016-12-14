f579702c83fc19b2a5af0ebfda7cc74d0ca946e7
awmyhr <awmyhr@gmail.com>
2016-12-14 12:51:42 -0500

    further refine comments, and set template version to 1.0

--- Templates/python-script.py
+++ Templates/python-script.py
@@ -23,10 +23,10 @@ Sphinx-flavored reStructuredText.
 #-- NOTE: We use optparse for compatibility with python < 2.7 as (the superior)
 #--       argparse wasn't standard until 2.7 (2.7 deprecates optparse)
 #--       As of 20161212 the template is coded for optparse only
-import optparse     # Argument parsing
-import os           # Misc. OS interfaces
-import sys          # System-specific parameters & functions
-import traceback    # Print/retrieve a stack traceback
+import optparse     #: Argument parsing
+import os           #: Misc. OS interfaces
+import sys          #: System-specific parameters & functions
+import traceback    #: Print/retrieve a stack traceback
 #===============================================================================
 #-- Third Party Imports
 #===============================================================================
@@ -36,17 +36,17 @@ import traceback    # Print/retrieve a stack traceback
 #-- TODO: Update meta vars
 __version__ = '0.1.0-alpha'
 __revised__ = '2016-12-14'
-__contact__ = 'awmyhr <awmyhr@gmail.com>'  # primary contact for support/?'s
+__contact__ = 'awmyhr <awmyhr@gmail.com>'  #: primary contact for support/?'s
 
 #-- The following few variables should be relatively static over life of script
-__author__ = 'awmyhr <awmyhr@gmail.com>'    # coder(s) of script
-__created__ = '2016-12-12'                  # date script originlly created
-__copyright__ = ''                          # Copyright short name
-__cononical_name__ = 'python-script.py'     # static name, *NOT* os.path.basename(sys.argv[0])
-__project_name__ = 'nixTools'               # name of overall project, if needed
-__project_home__ = 'https://github.com/awmyhr/nixTools' # where to find source/documentation
-__template_version__ = '0.1.0'              # version of template file used
-__docformat__ = 'reStructuredText en'       # attempted style for documentation
+__author__ = 'awmyhr <awmyhr@gmail.com>'    #: coder(s) of script
+__created__ = '2016-12-12'                  #: date script originlly created
+__copyright__ = ''                          #: Copyright short name
+__cononical_name__ = 'python-script.py'     #: static name, *NOT* os.path.basename(sys.argv[0])
+__project_name__ = 'nixTools'               #: name of overall project, if needed
+__project_home__ = 'https://github.com/awmyhr/nixTools' #: where to find source/documentation
+__template_version__ = '1.0.0'              #: version of template file used
+__docformat__ = 'reStructuredText en'       #: attempted style for documentation
 
 
 #===============================================================================
