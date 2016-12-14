b590be1c2332fbcd4440812ee76af6174af801d6
awmyhr <awmyhr@gmail.com>
2016-12-14 12:31:49 -0500

    converted Doxygen-style documentation comments to Sphinx/reStructuredText-style

--- Templates/shell-script.sh
+++ Templates/shell-script.sh
@@ -2,22 +2,28 @@
 #-- NOTE: Default to POSIX shell/mode. Use some level of thought before
 #--       switching to bash
 #===============================================================================
-## @file      shell-script.sh
-## @brief     This is a template for POSIX shell scripts
-## @details   Summary of script purpose/use
-## @author    awmyhr <awmyhr@gmail.com>
-## @copyright (If applicable)
-## @par       Non-standard Requirements:
-## @par       Non-standard Options:
-##
-#===============================================================================
-## @note    For guidance, please refer to:
-##          - [POSIX.1-2008 Base Specification, Issue 7](http://pubs.opengroup.org/onlinepubs/9699919799/nframe.html)
-##          - [GNU Coding Standard](http://www.gnu.org/prep/standards/)
-## @note    ShellCheck should be used, accepted issues should be marked w/disable comment
-## @warning Option parsing is currently less-than-ideal
-#===============================================================================
-## @par     Settings
+#:"""
+#: .. progrom:: shell-script.sh
+#:    :synopsis: This is a template for POSIX shell scripts
+#:
+#: .. codeauthor:: awmyhr <awmyhr@gmail.com>
+#: .. copyright:: (If applicable)
+#:
+#: .. note:: 
+#:     For guidance, please refer to:
+#:
+#:      - [POSIX.1-2008 Base Specification, Issue 7](http://pubs.opengroup.org/onlinepubs/9699919799/nframe.html)
+#:      - [GNU Coding Standard](http://www.gnu.org/prep/standards/)
+#:
+#: .. note::
+#:     ShellCheck should be used, accepted issues should be marked w/disable comment
+#:
+#: .. warning::
+#:     Option parsing is currently less-than-ideal
+#:
+#: This is where a long, verbose description of the script can go, using
+#: Sphinx-flavored reStructuredText.
+#:"""
 #===============================================================================
 #-- Variables which are meta for the script should be dunders (__varname__)
 #-- TODO: UPDATE meta vars
@@ -32,23 +38,24 @@ __cononical_name__='shell-script.sh'     # static name, *NOT* ${0}
 __project_name__='nix-Tools'             # name of overall project, if needed
 __project_home__='https://github.com/awmyhr/nixTools' # where to find source/documentation
 __template_version__='0.6.0'             # version of template file used
-__docformat__='Doxygen'                  # attempted style for documentation
+__docformat__='reStructuredText en'      # attempted style for documentation
 
 #-- The following are string formats (prepended with strfmt_)
 #-- NOTE: (you can ignore ShellCheck [SC2059] errors in lines where these are used)
 strfmt_error="${__cononical_name__}: %s\n"
 
 #===============================================================================
-## @par     Functions
-#===============================================================================
-
-#===============================================================================
-## @fn      usage()
-## @brief   Provides usage information
-## @note GNU does not recommend short options for help/version.
-##       I do not recommend short option for, nor display of, debug option.
-##       but I do recommend short option for help.
 _usage() {
+    #"""
+    #: .. function:: _usage()
+    #:
+    #: Provides usage information
+    #:
+    #: .. note::
+    #:     GNU does not recommend short options for help/version.
+    #:     I do not recommend short option for, nor display of, debug option.
+    #:     but I do recommend short option for help.
+    #:"""
     #-- TODO: UPDATE usage text.
     printf 'Usage: %s [options]\n\n' "${0}"
     printf 'Options:\n'
@@ -61,12 +68,19 @@ _usage() {
 }
 
 #===============================================================================
-## @fn      version()
-## @brief   Provides version information
-## @note GNU guidelines dictate adding copyright and license info (see commented code.)
-## @warning HOWEVER, this may not always be desierable.
-##          If not, REMOVE these lines -- do NOT leave them commented!
 _version() {
+    #:"""
+    #: .. function:: _version()
+    #:
+    #: Provides version information
+    #:
+    #: .. note::
+    #:     GNU guidelines dictate adding copyright and license info
+    #:     (see commented code.)
+    #: .. warning::
+    #:     HOWEVER, this may not always be desierable.
+    #:     If not, REMOVE these lines -- do NOT leave them commented!
+    #:"""
     printf '%s (%s) %s\n' "${__cononical_name__}" "${__project_name__}" "${__version__}"
     #-- TODO: UPDATE license
     # printf 'Copyright (c) 2016 awmyhr\n'
@@ -76,10 +90,15 @@ _version() {
 }
 
 #===============================================================================
-## @fn      debug_info()
-## @brief   Provides meta info for debug-level output
-## @note We set script_path here to avoid calling cd/dirname/pwd when not really needed
 _debug_info() {
+    #:"""
+    #: .. function:: _debug_info()
+    #:
+    #: Provides meta info for debug-level output
+    #:
+    #: .. note::
+    #:     We set script_path here to avoid calling cd/dirname/pwd when not really needed
+    #:"""
     # shellcheck disable=SC1007
     script_path="$(CDPATH= cd -- "$(dirname -- "${0}")" && pwd )"
 
@@ -94,31 +113,44 @@ _debug_info() {
 }
 
 #===============================================================================
-## @fn      exit_error()
-## @brief   Report error and exit
-## @param   error_number  Number to use for Exit Code
-## @param   error_string  Short description of error
-## @note    You may want to use ${LINENO} in the string this is called with.
-## @warning This function exits the script
 exit_error() {
+    #:"""
+    #: .. function:: exit_error()
+    #:
+    #: Report error and exit
+    #:
+    #: :param int error_number:  Number to use for Exit Code
+    #: :param str error_string:  Short description of error
+    #: .. note::
+    #:     You may want to use ${LINENO} in the string this is called with.
+    #: .. warning::
+    #:     This function exits the script
+    #:"""
     # shellcheck disable=SC2059
     printf "${strfmt_error}" "${2}" 1>&2
     exit "${1}"
 }
 
 #===============================================================================
-## @fn      exit_trap()
-## @brief   On terminal traps, run this before exit_error
-## @param   trap_line   Line number trap caught on
-## @param   trap_name   Short name of trap
 _exit_trap() {
+    #:"""
+    #: .. function:: exit_trap()
+    #:
+    #: On terminal traps, run this before exit_error
+    #:
+    #: :param int trap_line: Line number trap caught on
+    #: :param str trap_name: Short name of trap
+    #:"""
     exit_error "${?}" "caught trap ${2} at line ${1}; exiting"
 }
 
 #===============================================================================
-## @fn      exit_clean()
-## @brief   Clean up everything and exit gracefully
 exit_clean() {
+    #:"""
+    #: .. function:: exit_clean()
+    #:
+    #: Clean up everything and exit gracefully
+    #:"""
     [ "${script_debug}" = true ] && {
         set +o xtrace
         script_debug=false
@@ -130,16 +162,18 @@ exit_clean() {
 }
 
 #===============================================================================
-## @fn      init()
-## @brief   Check for requirements, other set-up stuff
 _init() {
+    #:"""
+    #: .. function:: init()
+    #:
+    #: Check for requirements, other set-up stuff
+    #:"""
+    #-- TODO: 'Initilize', check for requirements, etc...
     :
 }
 
 #===============================================================================
-## @par     Preperation
-#===============================================================================
-
+#-- Check for debug flag & process
 if [ "${*#*--debug}" != "${*}" ]; then
     _debug_info
     script_debug=true
@@ -152,15 +186,21 @@ fi
 set -o errexit  # Exit if any statement returns non-true value
 set -o nounset  # Exit if attempt to use an uninitialised variable
 
-## @note    Setting traps seprately for 2 reasons:
-##          -# Avoid calling "$(kill -l $((trap_num - 128)))" in exti_trap
-##          -# Incase we want to handle them differently in the future
-##
-## @bug     Passing LINENO may not be working as expected, need more real-world tests
+#:"""
+#: .. note::
+#:     Setting traps seprately for 2 reasons:
+#:
+#:      - Avoid calling "$(kill -l $((trap_num - 128)))" in exti_trap
+#:      - Incase we want to handle them differently in the future
+#:
+#: .. describe:: Bug
+#:     Passing LINENO may not be working as expected, need more real-world tests
+#:"""
 trap '_exit_trap ${LINENO} INT'  INT
 trap '_exit_trap ${LINENO} TERM' TERM
 trap '_exit_trap ${LINENO} HUP'  HUP
 
+#-- Process options (ignore --debug, as we already checked)
 [ "${#}" -gt 0 ] && while :; do
     case "${1}" in
         --debug)    ;;
@@ -177,15 +217,8 @@ done
 _init
 
 #===============================================================================
-## @par     Main
-#===============================================================================
-
 #-- TODO: Do something more interesting here...
 printf '%s\n' 'Hello world!'
 
-
 #===============================================================================
-## @par     Post
-#===============================================================================
-
 exit_clean
