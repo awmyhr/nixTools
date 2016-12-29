1bcc0b3553ed4432edc5c2aa4fc9317dd113c550
awmyhr <awmyhr@gmail.com>
2016-12-29 14:06:28 -0500

    change html template to solar

--- Doc/conf.py
+++ Doc/conf.py
@@ -16,6 +16,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import solar_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.pylink'))
@@ -127,7 +128,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'solar_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -136,7 +137,7 @@ html_theme = 'classic'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = [solar_theme.theme_path]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
